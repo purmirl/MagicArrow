929e7ed23054aeaa9d869229e7b9f575b0167e2a
purmirl <gmlrnjs1994@gmail.com>
2020-06-20 18:36:26 +0900

    start project

---
+++ src/main.py
@@ -0,0 +1,11 @@
+"""
+ Copyright 2020 PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+
+ Magic Arrow Project by PeTrA. 2020
+ MagicArrow 1.0
+
+ Packet Launcher :: ARP (Address Resolution Protocol)
+
+"""
+# PythonProjectStructure/src/main.py;
