9288ecd2a360e202b88b089296e5d50526fa1532
purmirl <gmlrnjs1994@gmail.com>
2020-06-21 18:31:09 +0900

    Studying Python Scapy

--- src/main.py
+++ src/main.py
@@ -4,8 +4,18 @@
 
  Magic Arrow Project by PeTrA. 2020
  MagicArrow 1.0
+ Language : Python3.8.2
+ Library : scapy-Python3
 
  Packet Launcher :: ARP (Address Resolution Protocol)
-
 """
+
 # PythonProjectStructure/src/main.py;
+
+from scapy.all import *
+
+packet = IP(dst = "8.8.8.8")/ICMP()
+packet.show()
+
+
+
