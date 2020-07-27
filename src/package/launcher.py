176cdbf27fb3b03283cea1c0f0df6cdd3eb6aa17
purmirl <gmlrnjs1994@gmail.com>
2020-07-27 22:26:10 +0900

    cui is modified, function.py is added, tests/package/main.py is added.

---
+++ src/package/launcher.py
@@ -0,0 +1,30 @@
+"""
+ Copyright 2020 PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+
+ Magic Arrow Project by PeTrA. 2020
+ MagicArrow 1.0
+ Language : Python3.8.2
+ Library : Scapy2.4.3
+
+ Packet Launcher :: ARP (Address Resolution Protocol)
+ ------
+ @ launcher.py
+    * launcher python code file --> engine code
+"""
+
+# MagicArrow/src/cui.py
+
+class launcher():
+
+    def __init__(self):
+        return
+
+    def exception_mac_error(_mac_address):
+        length = len(_mac_address)
+        if length > 17:
+            print("check the mac address size :(")
+            return
+
+        return
+
