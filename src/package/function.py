de2216ee97c004d256659bd7458b90662fe804d6
purmirl <gmlrnjs1994@gmail.com>
2020-07-29 22:59:39 +0900

    function.py is modified.

---
+++ src/package/function.py
@@ -0,0 +1,17 @@
+"""
+ Copyright 2020 PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+
+ Magic Arrow Project by PeTrA. 2020
+ MagicArrow 1.0
+ Language : Python3.8.2
+ Library : Scapy2.4.3
+
+ Packet Launcher :: ARP (Address Resolution Protocol)
+ ------
+ @ function.py
+    * function python code -> my utility function's code
+"""
+
+# MagicArrow/src/function.py
+
