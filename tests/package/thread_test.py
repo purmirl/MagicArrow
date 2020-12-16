a5f151f908483c3aee8e108ccb9cab55717e4ae2
purmirl <gmlrnjs1994@gmail.com>
2020-12-16 14:47:47 +0900

    thinking about the launcher structure.

--- tests/package/thread_test.py
+++ tests/package/thread_test.py
@@ -3,9 +3,15 @@
 """
 
 import time
+from threading import Thread
 
-def function():
+def function(_thread_name):
     count_number = 0
     while True:
-        print("count number : " + count_number)
+        print("count number : ", count_number)
+        count_number = count_number + 1
+
         time.sleep(2)
+
+thread_01 = Thread(target = function, args = ("purmirl", ))
+thread_01.start()
