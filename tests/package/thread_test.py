ce88a57b50f1bb23c880268f80fadc31b0e6bd3c
purmirl <gmlrnjs1994@gmail.com>
2020-12-16 17:44:37 +0900

    thinking about the launcher structure.

--- tests/package/thread_test.py
+++ tests/package/thread_test.py
@@ -6,12 +6,32 @@ import time
 from threading import Thread
 
 def function(_thread_name):
+    print(_thread_name + " function start.")
+
     count_number = 0
     while True:
+        if THREAD_KEY == -1:
+            break
         print("count number : ", count_number)
         count_number = count_number + 1
-
         time.sleep(2)
 
-thread_01 = Thread(target = function, args = ("purmirl", ))
-thread_01.start()
+    print(_thread_name + " function stop.")
+    return
+
+def main_function():
+    global THREAD_KEY
+    THREAD_KEY = 0
+
+    thread_01 = Thread(target=function, args=("purmirl", ))
+    thread_01.start()
+
+    key = input()
+
+    print("main_function key : ", key)
+    if key == "0":
+        THREAD_KEY = -1
+
+    return
+
+main_function()
